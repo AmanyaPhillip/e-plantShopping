abef33cf7a59f11bdfbd9c1476deba806e7cf9e7
amanyaphillip <amanyaphillip@hotmail.com>
2025-07-15 19:31:06 -0400

    working cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,29 +8,48 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+  const calculateTotalAmount = () => { 
+    let total = 0;
+    cart.forEach(item => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);   
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 1){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }else if (item.quantity==0){
+        handleRemove(item);
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalcost = 0 ; 
+    cart.forEach(element => {
+        totalcost+= parseFloat(item.cost.substring(1)) * item.quantity
+    });
+    return totalcost;
   };
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   return (
     <div className="cart-container">
